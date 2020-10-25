cbeef2241fce57d47b216eab7ac9697f0098917a
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2020-10-25 02:50:15 -0600

    Create NumericOperators.java

---
+++ NumericOperators.java
@@ -0,0 +1,12 @@
+public class NumericOperators {
+    public static void name(Strings[] args) {
+        int x=1;
+        int y=1;
+
+        int sum =x+y;
+
+        System.out.println(sum);
+        
+        
+    }
+}
